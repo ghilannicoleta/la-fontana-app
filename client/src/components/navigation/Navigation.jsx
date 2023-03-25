2ac146f9ad5cb1727de00eb802db2120e90f83df
Alteo96 <albuteodor1996@gmail.com>
2023-03-25 21:33:23 +0000

    Add component sticky navbar and temporary bg img

--- client/src/components/navigation/Navigation.jsx
+++ client/src/components/navigation/Navigation.jsx
@@ -1,3 +1,48 @@
 export default function Navigation() {
-  return <nav>Navigation</nav>;
+  return (
+    <nav className="flex space-x-24 p-16 justify-center bg-stone-900 bg-opacity-75 sticky top-0 z-50 text-white">
+      <button
+        type="button"
+        className="italic ... hover:text-yellow-300 delay-150"
+      >
+        Menu
+      </button>
+      <button
+        type="button"
+        className="italic ... hover:text-yellow-300 delay-150"
+      >
+        Shop
+      </button>
+      <button
+        type="button"
+        className="italic ... hover:text-yellow-300 delay-150"
+      >
+        Restaurants
+      </button>
+      <button
+        type="button"
+        className="italic ... hover:text-yellow-300 delay-150"
+      >
+        Delivery
+      </button>
+      <button
+        type="button"
+        className="italic ... hover:text-yellow-300 delay-150"
+      >
+        Contacts
+      </button>
+      <button
+        type="button"
+        className="italic ... hover:text-yellow-300 delay-150"
+      >
+        Login
+      </button>
+      <button
+        type="button"
+        className="italic ... hover:text-yellow-300 delay-150"
+      >
+        Cart
+      </button>
+    </nav>
+  );
 }
