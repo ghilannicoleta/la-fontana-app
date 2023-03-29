75b8c2b3f2385287e6ca1ddd90fcf19eaf84c152
ghilannicole <ghilannicoleta46@gmail.com>
2023-03-29 18:52:18 +0300

    add component Slide

--- client/src/components/navigation/LoginButton.jsx
+++ client/src/components/navigation/LoginButton.jsx
@@ -2,7 +2,7 @@ export default function LoginButton() {
   return (
     <button
       type="button"
-      className="italic ... hover:text-yellow-300 delay-150"
+      className="... italic delay-150 hover:text-yellow-300"
     >
       Login
     </button>
---
+++ client/src/components/slide/Slide.jsx
@@ -0,0 +1,70 @@
+import { useState, useEffect, useCallback } from "react";
+
+export default function Slide() {
+  const arrSlides = [
+    {
+      img: "https://images.unsplash.com/photo-1531664767453-dc714de6b3c4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1542181961-9590d0c79dab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1522336572468-97b06e8ef143?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2952&q=80",
+    },
+  ];
+  const [activeSlideIndex, setActiveSlideIndex] = useState(0);
+
+  const handleNextSlide = useCallback(
+    (slideIndex) => {
+      let curentIndex = slideIndex;
+
+      if (curentIndex === arrSlides.length) {
+        curentIndex = 0;
+      }
+
+      if (curentIndex < 0) {
+        curentIndex = arrSlides.length - 1;
+      }
+
+      setActiveSlideIndex(curentIndex);
+    },
+    [arrSlides.length]
+  );
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      handleNextSlide(activeSlideIndex + 1);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, [activeSlideIndex, handleNextSlide]);
+
+  return (
+    <div className="flex h-screen w-screen">
+      {arrSlides.map((slide, index) => (
+        <img
+          src={slide.img}
+          key={slide.id}
+          alt="img"
+          className={`h-full w-full object-cover object-bottom ${
+            activeSlideIndex === index ? "block" : "hidden"
+          }`}
+        />
+      ))}
+
+      <button
+        type="button"
+        className="absolute left-16 top-1/2 text-7xl text-white"
+        onClick={() => handleNextSlide(activeSlideIndex - 1)}
+      >
+        &#10094;
+      </button>
+      <button
+        type="button"
+        className="absolute right-16 top-1/2  text-7xl text-white"
+        onClick={() => handleNextSlide(activeSlideIndex + 1)}
+      >
+        &#10095;
+      </button>
+    </div>
+  );
+}
