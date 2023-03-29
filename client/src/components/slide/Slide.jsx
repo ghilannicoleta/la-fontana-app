0e7e21a0ccc38ccc0c5aceedeec00b0465ee72b6
ghilannicole <ghilannicoleta46@gmail.com>
2023-03-29 19:05:58 +0300

    add css properties at Slide component

--- client/src/components/slide/Slide.jsx
+++ client/src/components/slide/Slide.jsx
@@ -39,7 +39,7 @@ export default function Slide() {
   }, [activeSlideIndex, handleNextSlide]);
 
   return (
-    <div className="flex h-screen w-screen">
+    <div className="absolute top-0 flex h-screen w-screen">
       {arrSlides.map((slide, index) => (
         <img
           src={slide.img}
