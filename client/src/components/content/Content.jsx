40a63f5e045fb6a15e999b5818fb4a04d4fdf28f
MaryPirtu <maria.pirtu@hotmail.com>
2023-03-29 17:42:46 +0100

    Insert elements in ItemCard

--- client/src/components/content/Content.jsx
+++ client/src/components/content/Content.jsx
@@ -1,3 +1,38 @@
+import ItemCard from "./ItemCard";
+
 export default function Content() {
-  return <div>Content</div>;
+  return (
+    <div className="flex">
+      <ItemCard
+        src="https://www.andys.md/public/menu/thumbs/version_220x310x1/be45637985ce751c07c4bd2eeef1574e.jpg"
+        alt="pizza"
+        title="Pizza BBQ"
+        price={12.99}
+      />
+      <ItemCard
+        src="https://www.andys.md/public/menu/thumbs/version_220x310x1/be45637985ce751c07c4bd2eeef1574e.jpg"
+        alt="pizza"
+        title="Pizza BBQ"
+        price={12.99}
+      />
+      <ItemCard
+        src="https://www.andys.md/public/menu/thumbs/version_220x310x1/be45637985ce751c07c4bd2eeef1574e.jpg"
+        alt="pizza"
+        title="Pizza BBQ"
+        price={12.99}
+      />
+      <ItemCard
+        src="https://www.andys.md/public/menu/thumbs/version_220x310x1/be45637985ce751c07c4bd2eeef1574e.jpg"
+        alt="pizza"
+        title="Pizza BBQ"
+        price={12.99}
+      />
+      <ItemCard
+        src="https://www.andys.md/public/menu/thumbs/version_220x310x1/be45637985ce751c07c4bd2eeef1574e.jpg"
+        alt="pizza"
+        title="Pizza BBQ"
+        price={12.99}
+      />
+    </div>
+  );
 }
