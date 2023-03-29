8e16ba520f406719d80515e9b5bdd5fb3c87a8e2
Alteo96 <121298261+Alteo96@users.noreply.github.com>
2023-03-29 14:28:22 +0100

    Teo splitting components navbar (#17)
    * Delete Cart component
    
    * create login.jsx cart.jsx and import as sep comp
    
    * add one line at the end comp and add camelCase to Navigation
    
    * change camelCase in comp

--- client/src/components/navigation/Navigation.jsx
+++ client/src/components/navigation/Navigation.jsx
@@ -1,3 +1,7 @@
+import Login from "./Login";
+
+import Cart from "./Cart";
+
 export default function Navigation() {
   return (
     <nav className="flex space-x-24 p-16 justify-center bg-stone-900 bg-opacity-75 sticky top-0 z-50 text-white">
@@ -31,18 +35,8 @@ export default function Navigation() {
       >
         Contacts
       </button>
-      <button
-        type="button"
-        className="italic ... hover:text-yellow-300 delay-150"
-      >
-        Login
-      </button>
-      <button
-        type="button"
-        className="italic ... hover:text-yellow-300 delay-150"
-      >
-        Cart
-      </button>
+      <Login />
+      <Cart />
     </nav>
   );
 }
---
+++ client/src/components/navigation/login.jsx
@@ -0,0 +1,10 @@
+export default function Login() {
+  return (
+    <button
+      type="button"
+      className="italic ... hover:text-yellow-300 delay-150"
+    >
+      Login
+    </button>
+  );
+}
