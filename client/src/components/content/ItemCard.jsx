72e6d3db8c20f751cff4f96c82492b0cc508a122
MaryPirtu <121303617+MaryPirtu@users.noreply.github.com>
2023-03-31 13:36:06 +0100

    Content item card style (#25)
    * Add font color
    
    * Change image margin & props
    
    * Style Item Card
    
    * Display items in Content component

--- client/src/components/content/ItemCard.jsx
+++ client/src/components/content/ItemCard.jsx
@@ -1,10 +1,10 @@
-export default function ItemCard({ src, alt, title, price }) {
+export default function ItemCard({ item }) {
   return (
-    <div className="w-64 rounded-lg p-2 hover:border-2 hover:border-solid hover:border-yellow-500">
-      <img className="my-1 ml-2 rounded-lg" src={src} alt={alt} />
-      <p className="text-center text-white"> {title} </p>
-      <div className="flex justify-between rounded-lg border-2 border-solid border-yellow-500 p-0.5 pr-2 pl-2 text-white hover:bg-amber-400 hover:text-black">
-        <p>${price}</p>
+    <div className=" w-64 rounded-lg border-2 border-solid border-transparent p-2 hover:border-yellow-500">
+      <img className="my-1 rounded-lg" src={item.src} alt={item.alt} />
+      <p className="p-2"> {item.title} </p>
+      <div className="flex justify-between rounded-lg border-2 border-solid border-yellow-500 p-2 pr-2 pl-2 hover:bg-amber-400 hover:text-black">
+        <p>$ {item.price}</p>
         <button type="button">Order</button>
       </div>
     </div>
