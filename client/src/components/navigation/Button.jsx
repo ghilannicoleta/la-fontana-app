13adb97291fc69d87f0f4a4ff12ebec74d9f03d2
ghilannicole <ghilannicoleta46@gmail.com>
2023-03-31 17:29:39 +0300

    change style css

--- client/src/components/navigation/Button.jsx
+++ client/src/components/navigation/Button.jsx
@@ -1,6 +1,9 @@
 export default function Button({ title }) {
   return (
-    <button type="button" className="italic delay-150 hover:text-yellow-300">
+    <button
+      type="button"
+      className="italic transition  hover:text-colorYellow hover:delay-100"
+    >
       {title}
     </button>
   );
