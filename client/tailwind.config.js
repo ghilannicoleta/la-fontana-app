dc5ae57237f67dbe7962d725f8b67eb07e2144b5
ghilannicole <ghilannicoleta46@gmail.com>
2023-03-31 17:27:00 +0300

    add customized colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      colorYellow: "rgb(253 197 0)",
+      stone: "rgb(28 25 23)",
+      transparent: "transparent",
+    },
     extend: {},
   },
   plugins: [],
