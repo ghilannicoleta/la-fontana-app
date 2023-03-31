e8bd6a7961c43bbca51efd147d5cb1e736842f23
MaryPirtu <maria.pirtu@hotmail.com>
2023-03-31 13:15:11 +0100

    Change image margin & props

--- client/src/components/content/ItemCard.jsx
+++ client/src/components/content/ItemCard.jsx
@@ -1,10 +1,10 @@
-export default function ItemCard({ src, alt, title, price }) {
+export default function ItemCard({ item }) {
   return (
     <div className="w-64 rounded-lg p-2 hover:border-2 hover:border-solid hover:border-yellow-500">
-      <img className="my-1 ml-2 rounded-lg" src={src} alt={alt} />
-      <p className="text-center text-white"> {title} </p>
+      <img className="my-1 rounded-lg" src={item.src} alt={item.alt} />
+      <p className="text-center text-white"> {item.title} </p>
       <div className="flex justify-between rounded-lg border-2 border-solid border-yellow-500 p-0.5 pr-2 pl-2 text-white hover:bg-amber-400 hover:text-black">
-        <p>${price}</p>
+        <p>${item.price}</p>
         <button type="button">Order</button>
       </div>
     </div>
