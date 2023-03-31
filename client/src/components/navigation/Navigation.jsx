5b8f6078e5065231a5a458916f6ccc8699f673f2
ghilannicole <ghilannicoleta46@gmail.com>
2023-03-31 17:28:38 +0300

    add logo img

--- client/src/components/navigation/Navigation.jsx
+++ client/src/components/navigation/Navigation.jsx
@@ -1,10 +1,14 @@
 import LoginButton from "./LoginButton";
 import CartButton from "./CartButton";
 import Button from "./Button";
+import logo from "../../assets/pizza.png";
 
 export default function Navigation() {
   return (
-    <nav className="sticky top-0 z-50 flex justify-center space-x-24 bg-stone-900 bg-opacity-75 p-16 text-white">
+    <nav className="sticky top-0 z-50 flex justify-center space-x-24 bg-stone bg-opacity-80 p-9">
+      <div className="w-36">
+        <img alt="Logo" src={logo} />
+      </div>
       <Button title="Menu" />
       <Button title="Shop" />
       <Button title="Restaurants" />
