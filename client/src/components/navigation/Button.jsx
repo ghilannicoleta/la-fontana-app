e2beb12e690dd2b90a69b1a22cf24eb350fafd53
ghilannicole <ghilannicoleta46@gmail.com>
2023-04-03 19:24:45 +0300

    add tag Link

--- client/src/components/navigation/Button.jsx
+++ client/src/components/navigation/Button.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Button({ title }) {
   return (
     <button
       type="button"
       className="italic transition  hover:text-colorYellow hover:delay-100"
     >
-      {title}
+      <Link to={`/${title.toLowerCase()}`}>{title}</Link>
     </button>
   );
 }
+//
--- client/src/components/navigation/Navigation.jsx
+++ client/src/components/navigation/Navigation.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import LoginButton from "./LoginButton";
 import CartButton from "./CartButton";
 import Button from "./Button";
@@ -8,7 +9,9 @@ export default function Navigation() {
     <div className="sticky top-0 z-50 flex w-full justify-center bg-stone bg-opacity-80">
       <nav className="flex w-8/12 justify-between space-x-2 py-9 text-lg">
         <div className="w-36">
-          <img alt="Logo" src={logo} />
+          <Link to="/">
+            <img alt="Logo" src={logo} />
+          </Link>
         </div>
         <Button title="Menu" />
         <Button title="Shop" />
