71e35d322a0d6d5d7be169e23855469b5b1bd296
Alteo96 <albuteodor1996@gmail.com>
2023-04-04 21:03:08 +0100

    add icon

--- client/src/components/navigation/LoginButton.jsx
+++ client/src/components/navigation/LoginButton.jsx
@@ -1,10 +1,12 @@
+import { FaUser} from "react-icons/fa";
+
 export default function LoginButton() {
   return (
     <button
       type="button"
       className="italic transition hover:text-colorYellow hover:delay-100"
     >
-      Login
+      <FaUser />
     </button>
   );
 }
