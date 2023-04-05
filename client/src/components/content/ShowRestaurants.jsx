e93f2e49b50e581464d1ff21173d54be0e34dec4
MaryPirtu <maria.pirtu@hotmail.com>
2023-04-05 16:34:32 +0100

    Edit restaurants components

--- client/src/components/content/ShowRestaurants.jsx
+++ client/src/components/content/ShowRestaurants.jsx
@@ -1,44 +1,55 @@
 import Restaurants from "./Restaurants";
 
 export default function ShowRestaurants() {
+  const infoRestaurats = [
+    {
+      city: "London",
+      number: "0784 967 53",
+      address: "10 Teape Close",
+      hours: "9:00-23:00",
+    },
+    {
+      city: "Manchester",
+      number: "0614 967 531",
+      address: "3 Westbourne Terrace, Hyde Park Gardens",
+      hours: "10:00-22:30",
+    },
+    {
+      city: "Birmingham",
+      number: "0614 967 531",
+      address: "25-29 Coventry St",
+      hours: "10:00-22:30",
+    },
+    {
+      city: "London",
+      number: "0784 967 53",
+      address: "10 Teape Close",
+      hours: "9:00-23:00",
+    },
+    {
+      city: "Liverpool",
+      number: "0614 967 531",
+      address: "182 Grays Inn Rd",
+      hours: "10:00-22:30",
+    },
+    {
+      city: "Manchester",
+      number: "0614 967 531",
+      address: "4 St James's Market",
+      hours: "10:00-22:3",
+    },
+  ];
+
   return (
     <div className="grid grid-cols-1 gap-3">
-      <Restaurants
-        city="London"
-        number="0784 967 531"
-        address="10 Teape Close"
-        hours="9:00-23:00"
-      />
-      <Restaurants
-        city="Manchester"
-        number="0614 967 531"
-        address="3 Westbourne Terrace, Hyde Park Gardens"
-        hours="10:00-22:30"
-      />
-      <Restaurants
-        city="Birmingham"
-        number="0614 967 531"
-        address="25-29 Coventry St"
-        hours="10:00-22:30"
-      />
-      <Restaurants
-        city="London"
-        number="0614 967 531"
-        address="69-73 St John St"
-        hours="10:00-22:30"
-      />
-      <Restaurants
-        city="Liverpool"
-        number="0614 967 531"
-        address="182 Grays Inn Rd"
-        hours="10:00-22:30"
-      />
-      <Restaurants
-        city="Manchester"
-        number="0614 967 531"
-        address="4 St James's Market"
-        hours="10:00-22:30"
-      />
+      {infoRestaurats.map((el) => (
+        <Restaurants
+          city={el.city}
+          number={el.number}
+          address={el.address}
+          hours={el.hours}
+        />
+      ))}
     </div>
   );
 }
