24b8c13c5fba253de44e888ff426c41cbc8f12e1
ghilannicoleta <79923380+ghilannicoleta@users.noreply.github.com>
2023-04-05 19:52:24 +0300

    Nicoleta/solve bag (#37)
    * delete react-icons
    
    * add svg-icons
    
    * delete components
    
    * add useEffect
    
    * add children
    
    * add Login component
    
    * add icons

--- client/src/components/navigation/Button.jsx
+++ client/src/components/navigation/Button.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
 
-export default function Button({ title }) {
+export default function Button({ title, children }) {
   return (
     <button
       type="button"
       className="transition  hover:text-colorYellow hover:delay-100"
     >
-      <Link to={`/${title.toLowerCase()}`}>{title}</Link>
+      <Link to={`/${title.toLowerCase()}`}>{children || title}</Link>
     </button>
   );
 }
--- client/src/routes/DeliveryPage.jsx
+++ client/src/routes/DeliveryPage.jsx
@@ -1,8 +1,13 @@
+import { useEffect } from "react";
 import Footer from "../components/footer/Footer";
 import Delivery from "../components/navigation/Delivery";
 import Navigation from "../components/navigation/Navigation";
 
 export default function MenuPage() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="App">
       <Navigation />
