0faf743f144178eb05586b5bb1fdeca5aa1e7937
ghilannicole <ghilannicoleta46@gmail.com>
2023-04-13 19:06:26 +0300

    add handling for new routes

--- server/src/index.js
+++ server/src/index.js
@@ -3,12 +3,20 @@ import express from "express";
 import bodyParser from "body-parser";
 import UserService from "./services/UserService.js";
 import initDb from "./database.js";
+import CartService from "./services/cartService.js";
+import cors from "cors";
+import ProductService from "./services/productService.js";
 
 dotenv.config();
 const app = express();
 app.use(bodyParser.json());
-const port = 3002;
+const port = process.env.PORT || 3003;
 initDb();
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+};
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -31,4 +39,26 @@ app.post("/user", async (req, res) => {
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
+});
+
+app.post("/api/cart", async (req, res) => {
+  const cart = new CartService();
+  const saveItem = await cart.addToCart(req.body);
+  res
+    .header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    )
+    .send(saveItem);
+});
+
+app.get("/api/products", async (req, res) => {
+  const productService = new ProductService();
+  const products = await productService.getAllProducts();
+  res
+    .header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    )
+    .send(products);
 });
