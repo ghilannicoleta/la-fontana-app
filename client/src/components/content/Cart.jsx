24a90611ed3694ee26ff37d56c3fdb91afc6af23
ghilannicole <ghilannicoleta46@gmail.com>
2023-04-14 18:30:10 +0300

    add delete and get cart

--- client/src/components/content/Cart.jsx
+++ client/src/components/content/Cart.jsx
@@ -1,18 +1,25 @@
+import { useEffect } from "react";
 import CartEmpty from "./CartEmpty";
 import CartContent from "./CartContent";
 import { useContentContext } from "../../context/CartContext";
+import Api from "../../api/api";
 
 export default function Cart() {
   const { selectedProducts, setSelectedProducts } = useContentContext();
 
-  const removeFromCart = (item) => {
-    const currentCartProducts = [...selectedProducts];
-    const productIndex = currentCartProducts.findIndex(
-      (el) => el.productData.title === item.title
-    );
-    currentCartProducts.splice(productIndex, 1);
-    setSelectedProducts(currentCartProducts);
+  const removeFromCart = async (item) => {
+    const removeItem = await Api.deleteFromCart(item);
+    setSelectedProducts(removeItem);
   };
+
+  async function getCartItems() {
+    const cartItems = await Api.getCart();
+    setSelectedProducts(cartItems);
+  }
+
+  useEffect(() => {
+    getCartItems();
+  }, []);
 
   return (
     <div className="text-1xl mx-auto mt-10 w-4/5 bg-bgSlate p-5 py-16 font-bold text-stone">
