82d3b44602c756397e755e96f0c644a7dc542296
MaryPirtu <maria.pirtu@hotmail.com>
2023-04-17 15:04:54 +0100

    Add slide pictures

--- client/src/components/slide/Slide.jsx
+++ client/src/components/slide/Slide.jsx
@@ -3,10 +3,19 @@ import { useState, useEffect, useCallback } from "react";
 export default function Slide() {
   const arrSlides = [
     {
+      img: "https://images.unsplash.com/photo-1531664767453-dc714de6b3c4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    },
+    {
       img: "https://images.pexels.com/photos/2332685/pexels-photo-2332685.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     },
     {
+      img: "https://images.unsplash.com/photo-1542181961-9590d0c79dab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    },
+    {
       img: "https://images.pexels.com/photos/3915857/pexels-photo-3915857.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1522336572468-97b06e8ef143?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2952&q=80",
     },
     {
       img: "https://images.pexels.com/photos/10790638/pexels-photo-10790638.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
